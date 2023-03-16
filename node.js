229a722c37cb814ed9206827821ad828638b980b
XavierStackTrek <xavier.stacktrek@gmail.com>
2023-03-17 04:45:01 +0800

    added route example to node js

--- node.js
+++ node.js
@@ -9,6 +9,12 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
+//a route for localhost:3000/test
+app.get('/test', (req, res) => {
+  res.send('hi im test');
+});
+
+
 // Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
