cf746cd1ab0188f57ca24126d5aa408088257fcd
XavierStackTrek <xavier.stacktrek@gmail.com>
2023-03-19 10:24:56 +0800

    Added axios

--- React/starterkit_react/src/App.js
+++ React/starterkit_react/src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+//we import the component from fetch_data.js. this component will fetch data from the database
 import Fetch_data from './fetch_data';
 
 function App() {
@@ -8,8 +9,8 @@ function App() {
       <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
-
-    {/* //this is the component that fetches data from the database */}
+  
+   {/* this is the component that we imported */}
     <Fetch_data/>
     </div>
   );
